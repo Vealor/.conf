d303448ad6ca9f3a6046a2acaf6dcb1ba2f8abfa
Vealor <jakob.m.roberts@gmail.com>
2022-03-28 17:25:17 -0700

    Update install

--- install.sh
+++ install.sh
@@ -25,7 +25,8 @@ brew bundle install
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 nvm install --lts
 
 # install oh-my-zsh plugins
